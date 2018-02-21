7a2d60e01c27b56cdd93408550f9b3e056ce45a6
Fairen <vacher.mathieu@gmail.com>
2018-02-22 00:52:27 +0100

    Update Readme, Setup CI

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# seed-component
-Angular Component Module Seed 
+# @fairen/components
+
+A Angular module seed for Components
+
+## Installation 
+```sh
+npm install @fairen/components --save
+yarn add @fairen/componentsmodels
+bower install @fairen/components --save
+```
+## Usage
+```typescript
+import { SeedModule } from '@fairen/components';
+```
+
+```html
+<ng-seed>
+</ng-seed>
+```
+
+## Test 
+```sh
+npm run test
+```
--- karma.conf.js
+++ karma.conf.js
@@ -8,15 +8,17 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require( 'karma-phantomjs-launcher' ),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-spec-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: [ 'html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
@@ -27,7 +29,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS','Chrome'],
     singleRun: false
   });
 };
